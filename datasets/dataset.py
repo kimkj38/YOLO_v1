24edf34a8e9c3aa8a63bbac1217d7f60695dd3a3
hy21116 <hy21116@naver.com>
2021-06-29 22:48:19 +0900

    pycharm 코드 컨벤션 맞추기 및 get_data 함수 구현

--- datasets/dataset.py
+++ datasets/dataset.py
@@ -1,22 +1,22 @@
-import sys
 import os
-import xmltodict #xmltodict 설치 필요
+import xmltodict  # xmltodict 설치 필요
 import os.path as pth
 import numpy as np
 from PIL import Image
-import torch
 from torch.utils.data import Dataset, DataLoader
 import torchvision.transforms as transforms
 
+
 class VOCdataset(Dataset):
-    def __init__(self, mode='train', resize=448, transforms=None, feature_size=7 , num_bboxes=2, num_classes=20):
+    def __init__(self, dataset_dir='./datasets', mode='train', resize=448, transforms=None, feature_size=7, num_bboxes=2, num_classes=20):
+        self.dataset_dir = dataset_dir
         self.mode = mode
         self.resize_factor = resize
         self.transform = transforms
         self.feature_size = feature_size
         self.num_bboxes = num_bboxes
         self.num_classes = num_classes
-        self.data_list = self.get_infos(mode = self.mode)
+        self.data_list = self.get_infos(mode=self.mode)
         self.image_f = './{}/VOCdevkit/VOC2007/JPEGImages/{}' 
 
     def __len__(self):
@@ -35,7 +35,7 @@ class VOCdataset(Dataset):
         if self.transform:
             image = self.transform(img)
 
-        if self.mode== 'train':
+        if self.mode == 'train':
             target = self.encode(bboxs, labels)
             return image, target
         else:
@@ -43,17 +43,18 @@ class VOCdataset(Dataset):
 
     def get_infos(self, mode='train'):
 
-        annot_f = './{}/VOCdevkit/VOC2007/Annotations'
+        # annot_f = '{}/VOCdevkit/VOC2007/Annotations'
+        annot_f = '{}\VOCdevkit\VOC2007\Annotations'
         
-        classes = ['person', 'bird', 'cat', 'cow', 'dog', 'horse', 
-                'sheep', 'aeroplane', 'bicycle', 'boat', 'bus', 'car', 
-                'motorbike', 'train', 'bottle', 'chair', 'dining table', 
-                'potted plant', 'sofa', 'tv/monitor' ]
+        classes = ['person', 'bird', 'cat', 'cow', 'dog', 'horse',
+                   'sheep', 'aeroplane', 'bicycle', 'boat', 'bus', 'car',
+                   'motorbike', 'train', 'bottle', 'chair', 'dining table',
+                   'potted plant', 'sofa', 'tv/monitor']
 
-        annot_dir = annot_f.format(mode) 
+        annot_dir = pth.join(self.dataset_dir, annot_f.format(mode))
         result = [] 
         for ano in [pth.join(annot_dir, ano) for ano in os.listdir(annot_dir)]: 
-            f = open(ano) # xml 파일 하나씩 읽어들임
+            f = open(ano)  # xml 파일 하나씩 읽어들임
             info = xmltodict.parse(f.read())['annotation'] 
             image_id = info['filename'] 
             image_size = np.asarray(tuple(map(int, info['size'].values()))[:2], np.int16) 
@@ -63,16 +64,16 @@ class VOCdataset(Dataset):
             bboxs = [] 
             for obj in box_objects: 
                 try: 
-                    labels.append(classes.index(obj['name'].lower())) # 0~19 사이
+                    labels.append(classes.index(obj['name'].lower()))  # 0~19 사이
                     bboxs.append(tuple(map(int, obj['bndbox'].values()))) 
                 except: pass 
             bboxs = np.asarray(bboxs, dtype=np.float64) 
             try: 
-                bboxs[:, [0,2]] /= w 
-                bboxs[:, [1,3]] /= h 
+                bboxs[:, [0, 2]] /= w
+                bboxs[:, [1, 3]] /= h
             except: pass 
-            if bboxs.shape[0] or mode=='test': 
-                result.append({'image_id':image_id, 'image_size':image_size, 'bboxs':bboxs, 'labels':labels}) 
+            if bboxs.shape[0] or mode == 'test':
+                result.append({'image_id': image_id, 'image_size': image_size, 'bboxs': bboxs, 'labels': labels})
         return result 
 
     def encode(self, bboxs, labels):    
@@ -83,25 +84,27 @@ class VOCdataset(Dataset):
 
         box_xy = (bboxs[:, 2:] + bboxs[:, :2]) / 2.0
         box_wh = (bboxs[:, 2:] - bboxs[:, :2])
-        target = np.zeros((S,S,N))
-        for b in range(bboxs.shape[0]): # ground truth 박스 수만큼 반복
+        target = np.zeros((S, S, N))
+        for b in range(bboxs.shape[0]):  # ground truth 박스 수만큼 반복
             xy, wh, label = box_xy[b], box_wh[b], labels[b]
-            ij = np.ceil(xy / cell_size) -1.0 # ceil: 소수점있으면 무조건 올림 4.1 -> 5
-            i,j = map(int, ij) # i,j: cell 번호 (0~6)
-            top_left = ij*cell_size # 각 cell의 좌상단 좌표
+            ij = np.ceil(xy / cell_size) - 1.0  # ceil: 소수점있으면 무조건 올림 4.1 -> 5
+            i,j = map(int, ij)  # i,j: cell 번호 (0~6)
+            top_left = ij*cell_size  # 각 cell의 좌상단 좌표
             xy_norm = (xy-top_left) / cell_size
                 
-            for k in range(B): # 한 cell당 두개의 박스
+            for k in range(B):  # 한 cell당 두개의 박스
                 a = 5 * k
-                target[j, i, a  :a+2] = xy_norm
+                target[j, i, a:a+2] = xy_norm
                 target[j, i, a+2:a+4] = wh
-                target[j, i, a+4    ] = 1.0
+                target[j, i, a+4] = 1.0
             target[j, i, 5*B + label] = 1.0
         return target 
 
-if __name__ is '__main__':
-    train_dataset =  VOCdataset(transforms=transforms.ToTensor(), mode='train')
-    test_dataset =  VOCdataset(transforms=transforms.ToTensor(), mode='test')
+
+def get_data(dataset_dir='./datasets'):
+    print('Load datasets...')
+    train_dataset = VOCdataset(dataset_dir, transforms=transforms.ToTensor(), mode='train')
+    test_dataset = VOCdataset(dataset_dir, transforms=transforms.ToTensor(), mode='test')
 
     train_loader = DataLoader(dataset=train_dataset,
                               batch_size=32,
@@ -110,4 +113,5 @@ if __name__ is '__main__':
     test_loader = DataLoader(dataset=test_dataset,
                              shuffle=False,
                              batch_size=32)
-                     
+
+    return train_loader, test_loader
