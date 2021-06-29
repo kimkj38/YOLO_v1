9de7197fd7bf44dccc3d64b681a4be301029b820
hy21116 <hy21116@naver.com>
2021-06-29 23:52:19 +0900

    Bug_fix: 파이썬 스크립트 실행 위한 경로 추가

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import sys
+sys.path.append('agent')
+sys.path.append('datasets')
+sys.path.append('models')
+sys.path.append('utils')
 import argparse
-from agent.train import train
+from agent import train
 
 parser = argparse.ArgumentParser(description='YOLO v1')
 parser.add_argument('--train', action='store_true')
